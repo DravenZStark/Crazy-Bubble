44fb96a081aea96f65179cbbbc4b15ae68ff96fe
DravenStark <dravenout1025@gmail.com>
2014-04-12 23:40:38 +0800

    The crazy bubble main branch

---
+++ CrazyBubble/WonScene.h
@@ -0,0 +1,15 @@
+//
+//  WonScene.h
+//  CrazyBubble
+//
+//  Created by admin on 14-4-5.
+//  Copyright (c) 2014年 admin. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface WonScene : SKScene
+-(id) initWithSize:(CGSize)size ;
+
+
+@end
---
+++ CrazyBubble/WonScene.m
@@ -0,0 +1,37 @@
+//
+//  WonScene.m
+//  CrazyBubble
+//
+//  Created by admin on 14-4-5.
+//  Copyright (c) 2014年 admin. All rights reserved.
+//
+
+#import "WonScene.h"
+#import "MyScene.h"
+
+@implementation WonScene
+-(id) initWithSize:(CGSize)size
+{
+    if (self=[super initWithSize:size]) {
+        
+        SKEmitterNode *wonderful=[NSKeyedUnarchiver unarchiveObjectWithFile:[[NSBundle mainBundle] pathForResource:@"fire" ofType:@"sks"]];
+         wonderful.position=CGPointMake(self.size.width/2, self.size.height/2);
+        [self addChild:wonderful];
+        // self.backgroundColor=[SKColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
+        NSString *message=@"You Won";
+        SKLabelNode *label=[SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        label.text=message;
+        label.fontSize=40;
+        label.position=CGPointMake(self.size.width/2, self.size.height/2);
+        label.fontColor=[SKColor blackColor];
+        [self addChild:label];
+        
+        [self runAction:[SKAction sequence:@[[SKAction waitForDuration:2.1],[SKAction runBlock:^{
+            SKTransition *reveal=[SKTransition fadeWithDuration:1.3];
+            SKScene *myScene=[[MyScene alloc] initWithSize:size];
+            [self.view presentScene:myScene transition:reveal];
+        }]]]];
+    }
+    return  self;
+}
+@end
